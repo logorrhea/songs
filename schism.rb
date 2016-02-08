37a6ccb2486c8473efca1eee16daac5b957f20de
Tyler Funk <tfunk@trilixgroup.com>
2016-02-08 16:55:11 -0600

    Started adding in slurs

--- schism.rb
+++ schism.rb
@@ -40,30 +40,32 @@ define :bass_triplets do
   sleep 0.5
 end
 
-play :d2, sustain: 7, attack: 0, decay: 2
-sleep 8
+# play :d2, sustain: 7, attack: 0, decay: 2
+# sleep 8
 
 use_synth :fm
 
-8.times do
+comment do
+  # 8.times do
   bass_triplets
 end
 
-live_loop :moar_triplets do
+comment do
+  # live_loop :moar_triplets do
   use_synth :fm
   bass_triplets
 end
 
 use_transpose 0
-use_synth :subpulse
-
+use_synth :fm
+=begin
 
 # I know the pieces fit
 play :f
 sleep 1
-play :e
+sus = play :e, sustain: 0.5, attack: 0, note_slide: 0
 sleep 0.5
-play :d
+control sus, note: :d
 sleep 0.5
 play :d
 sleep 0.5
@@ -77,7 +79,7 @@ sleep 1
 # 'Cause I watched
 play :d
 sleep 0.5
-play :e
+sus = play :e, sustain: 0.5, attack: 0, note_slide: 0
 sleep 0.5
 play :d
 sleep 0.5
@@ -87,39 +89,46 @@ sleep 1
 # ..them fall away
 play :a3
 sleep 0.5
-play :e
+sus = play :e, sustain: 0.5, attack: 0, note_slide: 0
+# play :e
 sleep 0.5
-play :d
+control sus, note: :d
+# play :d
 sleep 0.5
-play :d
+sus = play :d, sustain: 0.5, attack: 0, note_slide: 0
+# play :d
 sleep 0.5
-play :cs
+control sus, note: :cs
+# play :cs
 sleep 0.5
-play :cs
+sus = play :cs, sustain: 0.5, attack: 0, note_slide: 0
+# play :cs
 sleep 0.5
-play :d
+control sus, note: :d
+# play :d
 sleep 1
 
 
 # Mildewed and smoldering
 play :f
 sleep 1
-play :e
+sus = play :e, sustain: 0.5
+sleep 0.5
+control sus, note: :d
 sleep 0.5
 play :d
 sleep 0.5
-play :d
+sus = play :f, sustain: 0.5
 sleep 0.5
-play :f
+control sus, note: :e
 sleep 0.5
-play :e
+sus = play :e, sustain: 0.5
 sleep 0.5
-play :e
+control sus, note: :d
 sleep 0.5
 play :g
 sleep 1
 sleep 0.5
-
 
 # Fundamental differing
 play :e
@@ -133,12 +142,13 @@ sleep 0.5
 sleep 0.5
 play :e
 sleep 1
-play :d
+sus = play :d, sustain: 0.5
 sleep 0.5
-play :f
+control sus, note: :f
 sleep 0.5
 play :f, sustain: 0.5
 sleep 1.5
+=end
 
 # Pure intention juxtaposed will
 play :f
